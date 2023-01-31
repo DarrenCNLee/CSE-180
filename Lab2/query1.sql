7871a1f5cea879ecf6bb14ec1e50cc787553572b
DarrenCNLee <chihoofsj@gmail.com>
2023-01-30 19:00:26 -0800

    Update query1.sql

--- Lab2/query1.sql
+++ Lab2/query1.sql
@@ -1,11 +1,14 @@
 SELECT
-    DISTINCT vehicleLicensePlate,
-    ownerState,
-    expirationDate
+    DISTINCT v.vehicleLicensePlate,
+    v.ownerState,
+    o.expirationDate
 FROM
-    Vehicles
+    Vehicles v,
+    Owners o
 WHERE
-    vehicleState = 'CA'
-    AND year < 2021
-    AND ownerState != 'CA'
-    AND expirationDate IS NOT NULL;
+    v.vehicleState = 'CA'
+    AND v.year < 2021
+    AND v.ownerState != 'CA'
+    AND o.expirationDate IS NOT NULL
+    AND v.ownerState = o.ownerState
+    AND v.ownerLicenseID = o.ownerLicenseID;
