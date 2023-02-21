3c7f7b13b8fd7b644b6071ffd7fc5762f435a38f
DarrenCNLee <chihoofsj@gmail.com>
2023-02-21 14:31:26 -0800

    fixing join in createview.sql

--- Lab3/createview.sql
+++ Lab3/createview.sql
@@ -8,7 +8,8 @@ FROM
     Exits e1,
     Exits e2
 WHERE
-    e2.exitNum > e1.exitNum
+    e1.highwayNum = e2.highwayNum
+    AND e2.exitNum > e1.exitNum
     AND e2.mileMarker < e1.mileMarker
 GROUP BY
     e1.highwayNum,
