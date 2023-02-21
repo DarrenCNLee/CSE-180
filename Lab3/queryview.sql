9d93ede9b39d6cc6a5af73893ef1cf6df55b051e
DarrenCNLee <chihoofsj@gmail.com>
2023-02-21 14:46:44 -0800

    working on queryview.sql

--- Lab3/queryview.sql
+++ Lab3/queryview.sql
@@ -18,4 +18,38 @@ GROUP BY
     h.highwayNum,
     h.length;
 
+
 -- output of the query on the load data before deletions:
+--  highwaynum | length | openmisdirectedexitcount
+-- ------------+--------+--------------------------
+--           1 |  150.3 |                        3
+--           8 |   90.0 |                        2
+--          13 |   45.5 |                        4
+--          17 |  102.6 |                        4
+--         280 |  200.9 |                        3
+-- (5 rows)
+
+
+DELETE FROM
+    Exits
+WHERE
+    highwayNum = 17
+    AND exitNum = 34;
+
+DELETE FROM
+    Exits
+WHERE
+    highwayNum = 280
+    AND exitNum = 95;
+
+-- output of the query after the deletions:
+--  highwaynum | length | openmisdirectedexitcount
+-- ------------+--------+--------------------------
+--           1 |  150.3 |                        3
+--           8 |   90.0 |                        2
+--          13 |   45.5 |                        4
+--          17 |  102.6 |                        3
+-- (4 rows)
+
+
+-- Yes, the answer is different after deleting the tuples from Exits
