d255db4daa7e3a633b0c36cff3a3fe97b3137c51
DarrenCNLee <chihoofsj@gmail.com>
2023-02-21 13:48:16 -0800

    working on foreign.sql

---
+++ foreign.sql
@@ -0,0 +1,24 @@
+ALTER TABLE
+    Vehicles
+ADD
+    CONSTRAINT OwnerStateAndLicenseIDFK 
+    FOREIGN KEY(ownerState, ownerLicenseID) REFERENCES Owners
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE;
+
+ALTER TABLE 
+    Photos
+ADD 
+    CONSTRAINT VehicleStateAndLicensePlateFK
+    FOREIGN KEY(vehicleState,vehicleLicensePlate) REFERENCES Vehicles 
+        ON DELETE CASCADE
+        ON UPDATE SET NULL;
+
+ALTER TABLE
+    Photos 
+ADD 
+    CONSTRAINT CameraIDFK
+    FOREIGN KEY(cameraID) REFERENCES Cameras 
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT;
+        
