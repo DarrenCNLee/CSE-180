3ed2009ab5cbda44d7ec048d22f001a11231ff4f
DarrenCNLee <chihoofsj@gmail.com>
2023-02-21 14:40:33 -0800

    working on queryview.sql

---
+++ Lab3/queryview.sql
@@ -0,0 +1,21 @@
+SELECT
+    h.highwayNum,
+    h.length,
+    COUNT(*) AS openMisdirectedExitCount
+FROM
+    Highways h,
+    Exits e
+WHERE
+    e.isExitOpen
+    AND e.highwayNum = h.highwayNum
+    AND h.highwayNum IN (
+        SELECT
+            m.highwayNum
+        FROM
+            MisdirectedExitView
+    )
+GROUP BY
+    h.highwayNum,
+    h.length;
+
+-- output of the query on the load data before deletions:
