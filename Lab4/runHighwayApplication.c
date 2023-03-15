4906d37455d087d9d00f74f7c301ffb427e8311f
DarrenCNLee <chihoofsj@gmail.com>
2023-03-15 14:37:19 -0700

    Update runHighwayApplication.c

--- Lab4/runHighwayApplication.c
+++ Lab4/runHighwayApplication.c
@@ -445,6 +445,21 @@ int main(int argc, char **argv)
     // test with maxFinetotal of 165
     testDetermineSpeedingViolationsAndFines(conn, 165);
 
+
+
+
+testDetermineSpeedingViolationsAndFines(conn, 720);
+     testDetermineSpeedingViolationsAndFines(conn, 725);
+     testDetermineSpeedingViolationsAndFines(conn, 512);
+     testDetermineSpeedingViolationsAndFines(conn, 65);
+     testDetermineSpeedingViolationsAndFines(conn, 8);
+
+
+
+
+
+
+
     good_exit(conn);
     return 0;
 }
